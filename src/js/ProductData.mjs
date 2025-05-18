922410ea330c3ded69d07ea496b85b0cb87c1150
RLTB-Rommel <rommelaunario101@gmail.com>
2025-05-18 20:56:39 +0800

    update

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,21 +1,25 @@
+// src/js/ProductData.mjs
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error("Bad response from server");
   }
 }
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `/json/${this.category}.json`;
   }
+
   getData() {
     return fetch(this.path)
       .then(convertToJson)
       .then((data) => data);
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
