bf0c3bd20019802d0741746323a34153db16b711
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-07 03:54:34 +0800

    updae

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -1,18 +1,17 @@
-// wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
 // save data to local storage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -27,14 +26,31 @@ export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const product = urlParams.get(param);
-  return product
+  return product;
 }
 
 export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(template);
-  // if clear is true we need to clear out the contents of the parent.
   if (clear) {
     parentElement.innerHTML = "";
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+
+export function loadHeaderFooter() {
+  const header = document.querySelector("header");
+  const footer = document.querySelector("footer");
+
+  fetch("/partials/header.html")
+    .then((res) => res.text())
+    .then((html) => {
+      if (header) header.innerHTML = html;
+    });
+
+  fetch("/partials/footer.html")
+    .then((res) => res.text())
+    .then((html) => {
+      if (footer) footer.innerHTML = html;
+    });
 }
