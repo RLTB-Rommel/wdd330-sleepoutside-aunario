a27b3f0ccb590125fefce58043c564111a783248
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-09 00:21:20 +0800

    update

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -61,4 +61,24 @@ export function loadHeaderFooter() {
     })
     .catch((err) => console.warn("[Footer Error]", err.message));
 }
+export function alertMessage(message, scroll = true) {
+  const alert = document.createElement("div");
+  alert.classList.add("alert");
+  alert.innerHTML = `<span>${message}</span><span class="close-btn" style="float:right;cursor:pointer;">✖</span>`;
 
+  alert.addEventListener("click", function (e) {
+    if (
+      e.target.classList.contains("close-btn") ||
+      e.target.innerText === "✖"
+    ) {
+      alert.remove();
+    }
+  });
+
+  const main = document.querySelector("main");
+  main.prepend(alert);
+
+  if (scroll) {
+    window.scrollTo(0, 0);
+  }
+}
