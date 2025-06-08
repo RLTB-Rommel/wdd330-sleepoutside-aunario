924b6706a5a956d10b62b9af8c60a8e89930cd13
RLTB-Rommel <rommelaunario101@gmail.com>
2025-06-09 00:22:45 +0800

    update

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,5 +1,5 @@
 import ExternalServices from "./ExternalServices.mjs";
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, alertMessage } from "./utils.mjs";
 
 const taxRate = 0.06;
 
